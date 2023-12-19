2842bef453da18ef5396629bae7982559b016209
lomakina0612 <lomakina0612@gmail.com>
2023-12-19 09:55:51 +0100

    Part_1

--- PB_HW.py
+++ PB_HW.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import platform
 
 import platform
@@ -6,21 +7,34 @@ import aiohttp
 import asyncio
 
 
-async def main():
+async def main(days=1):
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=days)
+
+    url = "https://api.privatbank.ua/p24api/exchange_rates?json&date="
+    url += start_date.strftime("%d.%m.%Y")  # Format start date as dd.mm.YYYY
+    url += "&end_date="
+    url += end_date.strftime("%d.%m.%Y")  # Format end date as dd.mm.YYYY
+
     async with aiohttp.ClientSession() as session:
-        async with session.get(
-            "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
-        ) as response:
+        async with session.get(url) as response:
             print("Status:", response.status)
             print("Content-type:", response.headers["content-type"])
             print("Cookies: ", response.cookies)
             print(response.ok)
             result = await response.json()
-            return result
+
+            # Фильтрация результатов только для USD и EUR
+            filtered_result = [
+                rate
+                for rate in result["exchangeRate"]
+                if rate["currency"] in ["USD", "EUR"]
+            ]
+
+            return filtered_result
 
 
 if __name__ == "__main__":
-    if platform.system() == "Windows":
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    r = asyncio.run(main())
+    days_to_fetch = 7  # You can change this to the desired number of days
+    r = asyncio.run(main(days=days_to_fetch))
     print(r)
