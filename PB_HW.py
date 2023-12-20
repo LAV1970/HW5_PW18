992eaa142838c8a410d92e11f1e2b7fd0438323c
lomakina0612 <lomakina0612@gmail.com>
2023-12-20 07:23:37 +0100

    Modification_3

--- PB_HW.py
+++ PB_HW.py
@@ -1,58 +1,59 @@
-import json
-from datetime import datetime, timedelta
 import aiohttp
 import asyncio
 
 
-async def main(days=1):
-    end_date = datetime.now()
-    start_date = end_date - timedelta(days=days)
-
-    url = "https://api.privatbank.ua/p24api/exchange_rates?json&date="
-    url += start_date.strftime("%d.%m.%Y")  # Format start date as dd.mm.YYYY
-    url += "&end_date="
-    url += end_date.strftime("%d.%m.%Y")  # Format end date as dd.mm.YYYY
-
+async def fetch(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
-            print("Status:", response.status)
-            print("Content-type:", response.headers["content-type"])
-            print("Cookies: ", response.cookies)
-            print(response.ok)
-            result_text = await response.text()
-            print("API Response:", result_text)
+            return await response.json()
 
-            result = await response.json()
 
-            # Check if 'exchangeRate' key is present
-            if "exchangeRate" in result:
-                # Фильтрация результатов только для USD и EUR
-                filtered_result = [
-                    {
-                        rate["baseCurrency"]: {
-                            rate["currency"]: {
-                                "sale": float(
-                                    rate.get("saleRateNB", rate.get("saleRate", 0))
-                                ),
-                                "purchase": float(
-                                    rate.get(
-                                        "purchaseRateNB", rate.get("purchaseRate", 0)
-                                    )
-                                ),
-                            }
-                        }
+async def main(days):
+    if days > 10:
+        print("Error: You can request exchange rates for up to 10 days only.")
+        return
+
+    url = f"https://api.privatbank.ua/p24api/exchange_rates?json&date="
+    tasks = [fetch(f"{url}{i}") for i in range(days, 0, -1)]
+
+    try:
+        responses = await asyncio.gather(*tasks)
+    except Exception as e:
+        print(f"Error fetching data: {e}")
+        return
+
+    filtered_result = []
+
+    for response in responses:
+        if "exchangeRate" in response:
+            rates = response["exchangeRate"]
+            filtered_rates = {}
+
+            for rate in rates:
+                if rate["currency"] in ["USD", "EUR"]:
+                    currency_code = rate["currency"]
+                    filtered_rates[currency_code] = {
+                        "sale": rate["saleRateNB"],
+                        "purchase": rate["purchaseRateNB"],
                     }
-                    for rate in result["exchangeRate"]
-                    if rate["currency"] in ["USD", "EUR"]
-                ]
 
-                return filtered_result
-            else:
-                print("No data found in the response.")
-                return []
+            if filtered_rates:
+                date_key = response.get("date", "Unknown Date")
+                filtered_result.append({date_key: filtered_rates})
+
+    if not filtered_result:
+        print("No data found in the response.")
+        return
+
+    print(filtered_result)
 
 
 if __name__ == "__main__":
-    days_to_fetch = 7  # You can change this to the desired number of days
-    result = asyncio.run(main(days=days_to_fetch))
-    print(json.dumps(result, indent=2, ensure_ascii=False))
+    try:
+        days_to_fetch = int(
+            input("Enter the number of days for exchange rates (up to 10): ")
+        )
+    except ValueError:
+        print("Error: Please enter a valid number.")
+    else:
+        asyncio.run(main(days_to_fetch))
