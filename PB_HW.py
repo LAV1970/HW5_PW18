54bf8873a575ebd4645079f343c3474419f73b06
lomakina0612 <lomakina0612@gmail.com>
2023-12-21 07:22:34 +0100

    modify_15

--- PB_HW.py
+++ PB_HW.py
@@ -8,60 +8,62 @@ class CurrencyExchange:
         self.api_url = api_url
         self.max_days = max_days
 
-    async def fetch_exchange_rate(self, session, days):
-        end_date = datetime.now()
-        start_date = end_date - timedelta(days=days - 1)
-
+    async def fetch_exchange_rate(self, session, start_date, end_date):
         params = {
             "json": "",
             "date": "",
-            "start_date": start_date.strftime("%Y-%m-%d"),
-            "end_date": end_date.strftime("%Y-%m-%d"),
+            "start_date": start_date.strftime("%d.%m.%Y"),
+            "end_date": end_date.strftime("%d.%m.%Y"),
         }
 
         try:
             async with session.get(self.api_url, params=params) as response:
                 print(f"Запрос к API: {response.url}")
-            if response.status == 200:
-                return await response.json()
-            else:
-                error_text = await response.text()
-                print(f"Ошибка при запросе к API. Статус код: {response.status}")
-                print(f"Текст ошибки: {error_text}")
-                return None
+                if response.status == 200:
+                    return await response.json()
+                else:
+                    error_text = await response.text()
+                    print(f"Ошибка при запросе к API. Статус код: {response.status}")
+                    print(f"Текст ошибки: {error_text}")
+                    return None
         except aiohttp.ClientError as e:
             print(f"Ошибка при выполнении запроса: {e}")
-        return None
+            return None
 
     async def get_exchange_rates(self, days):
         async with aiohttp.ClientSession() as session:
-            exchange_rate = await self.fetch_exchange_rate(session, days)
+            end_date = datetime.now()
+            result = []
 
-            if exchange_rate:
-                result = []
-                for day_rate in exchange_rate:
+            for i in range(days):
+                start_date = end_date - timedelta(days=i)
+                exchange_rate = await self.fetch_exchange_rate(
+                    session, start_date, end_date
+                )
+
+                if exchange_rate:
+                    # Выводим структуру ответа перед обращением к 'date'
+                    print(exchange_rate)
+
+                    day_rate = exchange_rate[
+                        0
+                    ]  # Предполагаем, что ответ содержит только один элемент
                     filtered_result = [
                         {
-                            "date": day_rate["date"],
-                            "baseCurrency": rate["baseCurrency"],
-                            "currency": rate["currency"],
-                            "sale": rate.get("saleRate", rate["saleRateNB"]),
-                            "purchase": rate.get(
-                                "purchaseRate", rate["purchaseRateNB"]
-                            ),
+                            "date": day_rate.get("date"),
+                            "baseCurrency": day_rate.get("base_ccy"),
+                            "currency": day_rate.get("ccy"),
+                            "sale": day_rate.get("sale"),
+                            "purchase": day_rate.get("buy"),
                         }
-                        for rate in day_rate["exchangeRate"]
-                        if rate["currency"] in ["USD", "EUR"]
                     ]
                     result.extend(filtered_result)
 
-                return result
-            else:
-                return None
+            return result if result else None
 
 
 def main():
-    api_url = "https://api.privatbank.ua/p24api/exchange_rates"
+    api_url = "https://api.privatbank.ua/p24api/pubinfo"
     max_days = 10
 
     try:
